e3913d659707672aa27401d1e970b9a9d5abb434
System Administrator <root@iMac-Pro-de-jaumecarreras.local>
2022-05-08 21:23:18 +0200

    cuatro

--- Modulo1/blockchain.py
+++ Modulo1/blockchain.py
@@ -30,6 +30,8 @@ class Blockchain:
                  'timestamp' : str(datetime.datetime.now()),
                  'proof' : proof,
                  'previous_hash' : previous_hash}    
+        self.chain.append(block)
+        return block
     
                  
             
